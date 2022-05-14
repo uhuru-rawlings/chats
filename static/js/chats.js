92f59ae77a899437d88a28f3aa3027211cba4988
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-14 19:25:31 +0300

    added action buttons

--- static/js/chats.js
+++ static/js/chats.js
@@ -1,3 +1,10 @@
 const toogleAddContact = () => {
-    
+    let target = document.getElementById("addform");
+    if(target.style.display == "block"){
+        target.style.display = "none";
+        document.getElementById("addbtn").innerHTML = "<i class='fa-solid fa-plus'></i>"
+    }else{
+        target.style.display = "block";
+        document.getElementById("addbtn").innerHTML = "<i class='fa-solid fa-xmark'></i>"
+    }
 }
