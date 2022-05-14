4850d711a16250f20a1e8cedc55ebf6f5038d852
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-15 00:02:15 +0300

    added send messages

--- homepage/views.py
+++ homepage/views.py
@@ -7,22 +7,33 @@ from homepage.models import Contacts,Messages
 def chat_view(request):
     user = request.COOKIES['userd']
     if user:
-        if request.method == 'POST':
-            pass
+       pass
     else:
         return redirect('signin')
     try:
       id = request.COOKIES['chat']
       rchat = Contacts.objects.get(id = id)
-      
+      name_array = rchat.contactname.split(' ')
+      nam_sep = name_array[0][0] + name_array[1][0]
     except:
+      nam_sep = ''
       rchat = 'nouser'
     user = Registration.objects.get(contact = user)
     contacts = Contacts.objects.filter(user = user)
+    if request.method == 'POST':
+        if rchat != 'nouser':
+            sender = user
+            receiver = rchat
+            messages = request.POST['messagecontent']
+
+            new_message = Messages(sender = sender, receiver =  receiver, message = messages)
+            new_message.save()
+
     context = {
         'title':'chatapp | Home',
         'contacts':contacts,
-        'rchat':rchat
+        'rchat':rchat,
+        'nam_sep':nam_sep,
     }
     return render(request,"chart.html",context)
 
