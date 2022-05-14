ea56da5e3101105e329494dab0a9ed022baf97e9
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-14 23:17:19 +0300

    added contact image

--- chats/urls.py
+++ chats/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from authentication.views import registration_view
 from login.views import login_view
-from homepage.views import chat_view,addcontact_view
+from homepage.views import chat_view,addcontact_view,delete_contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +25,5 @@ urlpatterns = [
     path('', login_view, name="signin"),
     path('chats/', chat_view, name="chats"),
     path('chats/addcontact/', addcontact_view, name="contact"),
+    path('chats/contact/delete/<int:id>/', delete_contact, name="delete"),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -10,8 +10,11 @@ def chat_view(request):
             pass
     else:
         return redirect('signin')
+    user = Registration.objects.get(contact = user)
+    contacts = Contacts.objects.filter(user = user)
     context = {
-        'title':'chatapp | login',
+        'title':'chatapp | Home',
+        'contacts':contacts
     }
     return render(request,"chart.html",context)
 
@@ -20,8 +23,8 @@ def addcontact_view(request):
     user = request.COOKIES['userd']
     if user:
         if request.method == 'POST':
-            contactname = request.POST['']
-            contact = request.POST['']
+            contactname = request.POST['contactname']
+            contact = request.POST['phonenumber']
             user = Registration.objects.get(contact = user)
             check_contact = Contacts.objects.filter(user = user,contact = contact)
             if check_contact.exists():
@@ -33,3 +36,8 @@ def addcontact_view(request):
                 return redirect('/chats/')
     else:
         return redirect('signin')
+
+def delete_contact(request, id):
+    contact = Contacts.objects.get(id = id)
+    contact.delete()
+    return redirect('/chats/')
