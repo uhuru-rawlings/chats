b16fe0e17bac254c8bc96ac28f2444019c9a5a5e
uhururawlings40 <uhururawlings40@gmail.com>
2022-05-14 22:17:54 +0300

    added login function view

--- chats/urls.py
+++ chats/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from authentication.views import registration_view
-from login import login_view
+from login.views import login_view
 from homepage.views import chat_view
 
 urlpatterns = [
--- login/views.py
+++ login/views.py
@@ -5,7 +5,14 @@ from authentication.models import Registration
 
 # Create your views here.
 def login_view(request):
+    error = ''
+    success = ''
+    if request.method == 'POST':
+        contact = request.POST['contact']
+        password = request.POST['password']
     context = {
         'title':'chatapp | login',
+        'error':error,
+        'success':success
     }
     return render(request,"login.html",context)
